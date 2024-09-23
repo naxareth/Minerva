fdc1deadb5799bb62f024d8696423fecdb1a7b25
naxareth <acephilipdenulan12@gmail.com>
2024-09-23 15:43:33 +0800

    fixed layout and added navigations

---
+++ app/src/main/java/com/example/minerva_10/SignUpActivity.kt
@@ -0,0 +1,30 @@
+package com.example.minerva_10
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Navigate back to Login
+        val signUpButton: Button = findViewById(R.id.btSignUp)
+        signUpButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
